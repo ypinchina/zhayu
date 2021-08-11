e96e3dd0badc56978f948d41c1ba85eaf98012b8
ypinchina <batmanyp@foxmail.com>
2021-08-11 08:00:37 +0800

    新建全局headerNav组件

--- src/components/columnList.vue
+++ src/components/columnList.vue
@@ -1,28 +1,44 @@
 <!-- 组件说明 -->
 <template>
-  <ul>
-    <li v-for="item in list" :key="item.id">
-      <img :src="item.avatar" alt="avatar" />
-      <div>{{ item.title }}</div>
-      <div>{{ item.description }}</div>
-    </li>
-  </ul>
+  <div class="row">
+    <div v-for="item in columnList" :key="item.id" class="col-4 mb-4">
+      <div class="card h-100 shadow-sm">
+        <div class="card-body text-center">
+          <img :src="item.avatar" alt="avatar" class="rounded-circle border border-light w-25 my-3"/>
+          <h5 class="card-title">{{ item.title }}</h5>
+          <p class="card-text text-left">{{ item.description }}</p>
+          <a href="#" class="btn btn-outline-primary">进入专栏</a>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue'
+import { computed, defineComponent, PropType } from 'vue'
 export interface ColumnProps {
   id: number;
   title: string;
-  avatar: string;
+  avatar?: string;
   description: string;
 }
 export default defineComponent({
   props: {
     list: {
       type: Array as PropType<ColumnProps[]>,
-      require: true
+      required: true
     }
+  },
+  setup (props) {
+    const columnList = computed(() => {
+      return props.list.map(item => {
+        if (!item.avatar) {
+          item.avatar = require('@/assets/avatar_default.jpg')
+        }
+        return item
+      })
+    })
+    return { columnList }
   }
 })
 </script>
